2d28139344ffc84a95c6442f797d8c594e5a474c
Benjamin Gutierrez <benjamingutierrez0097@gmail.com>
2023-07-02 19:30:39 -1200

    this is the menu bar

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,27 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-export default function Home() {
-  return (
-    <>
-    <h1>Weather App</h1>
-    </>
-  )
+
+async function getData(city) {
+  const response = await fetch (`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=67fe6a6f66b21320a216fea2c1e49ad5`)
+  const data = await response.json();
+  console.log(data);
 }
+
+
+function Home() {
+
+  const ciudad = "cochabamba";
+  getData(ciudad);
+
+  return (
+    <div className="time">
+      <div className='botones'>
+        <button>Search places</button>
+        <button>center</button>
+      </div>
+    </div>
+  );
+}
+
+export default Home
